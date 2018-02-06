de95b4424ae06edec5182f8e4f9b9e1fd0cee190
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2018-02-06 00:45:34 +0000

    Add form to allow item selection to hide or show on OBS

---
+++ ButtonDeck/Forms/ActionHelperForms/OBS/OBSSceneItemVisibilityHelper.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ButtonDeck.Forms.ActionHelperForms.OBS
+{
+    partial class OBSSceneItemVisibilityHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appBar1 = new NickAc.ModernUIDoneRight.Controls.AppBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.modernButton3 = new NickAc.ModernUIDoneRight.Controls.ModernButton();
+            this.modernButton2 = new NickAc.ModernUIDoneRight.Controls.ModernButton();
+            this.SuspendLayout();
+            // 
+            // appBar1
+            // 
+            this.appBar1.CastShadow = true;
+            this.appBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.appBar1.IconVisible = false;
+            this.appBar1.Location = new System.Drawing.Point(1, 33);
+            this.appBar1.Name = "appBar1";
+            this.appBar1.Size = new System.Drawing.Size(482, 50);
+            this.appBar1.TabIndex = 0;
+            this.appBar1.Text = "appBar1";
+            this.appBar1.TextFont = new System.Drawing.Font("Segoe UI", 14F);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(9, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Scene Name";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 117);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(458, 29);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.Location = new System.Drawing.Point(9, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Scene Item";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(13, 173);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(458, 29);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // modernButton3
+            // 
+            this.modernButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.modernButton3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.modernButton3.Location = new System.Drawing.Point(231, 211);
+            this.modernButton3.Name = "modernButton3";
+            this.modernButton3.Size = new System.Drawing.Size(117, 39);
+            this.modernButton3.TabIndex = 10;
+            this.modernButton3.Text = "Cancel";
+            this.modernButton3.UseVisualStyleBackColor = true;
+            this.modernButton3.Click += new System.EventHandler(this.ModernButton3_Click);
+            // 
+            // modernButton2
+            // 
+            this.modernButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.modernButton2.Location = new System.Drawing.Point(354, 211);
+            this.modernButton2.Name = "modernButton2";
+            this.modernButton2.Size = new System.Drawing.Size(117, 39);
+            this.modernButton2.TabIndex = 11;
+            this.modernButton2.Text = "OK";
+            this.modernButton2.UseVisualStyleBackColor = true;
+            this.modernButton2.Click += new System.EventHandler(this.ModernButton2_Click);
+            // 
+            // OBSSceneItemVisibilityHelper
+            // 
+            this.AcceptButton = this.modernButton2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.modernButton3;
+            this.ClientSize = new System.Drawing.Size(484, 263);
+            this.ColorScheme.PrimaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(119)))), ((int)(((byte)(189)))));
+            this.ColorScheme.SecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(75)))), ((int)(((byte)(120)))));
+            this.Controls.Add(this.modernButton3);
+            this.Controls.Add(this.modernButton2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.appBar1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "OBSSceneItemVisibilityHelper";
+            this.Text = "OBS Scene Item Visibility - Editor";
+            this.Load += new System.EventHandler(this.OBSSceneItemVisibilityHelper_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NickAc.ModernUIDoneRight.Controls.AppBar appBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private NickAc.ModernUIDoneRight.Controls.ModernButton modernButton3;
+        private NickAc.ModernUIDoneRight.Controls.ModernButton modernButton2;
+    }
+}
---
+++ ButtonDeck/Forms/ActionHelperForms/OBS/OBSSceneItemVisibilityHelper.cs
@@ -0,0 +1,74 @@
+﻿using NickAc.Backend.Objects.Implementation.DeckActions.OBS;
+using NickAc.Backend.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonDeck.Forms.ActionHelperForms.OBS
+{
+    public partial class OBSSceneItemVisibilityHelper : TemplateForm
+    {
+        public SceneItemVisibilityAction ModifiableAction { get; set; }
+
+        private void CloseWithResult(DialogResult result)
+        {
+            DialogResult = result;
+            Close();
+        }
+
+        public OBSSceneItemVisibilityHelper()
+        {
+            InitializeComponent();
+        }
+
+        private void OBSSceneItemVisibilityHelper_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = ModifiableAction.SceneName;
+            comboBox2.Text = ModifiableAction.SceneItem;
+            Thread th = new Thread(() => {
+                if (!OBSUtils.IsConnected)
+                    OBSUtils.ConnectToServer();
+                if (OBSUtils.IsConnected) {
+                    Thread th2 = new Thread(() => {
+                        var scenes = OBSUtils.GetOBSScenes();
+                        comboBox1.Invoke(new Action(() => {
+                            comboBox1.Items.AddRange(scenes.Select(c => c.SceneName).ToArray());
+                            scenes.All(s => {
+                                comboBox1.TextChanged += (ss, ee) => {
+                                    if (comboBox1.Text == s.SceneName) {
+                                        comboBox2.Items.Clear();
+                                        comboBox2.Items.AddRange(s.SceneItems.Select(cc => cc.ItemName).ToArray());
+                                    }
+                                };
+                                return true;
+                            });
+                            comboBox1.Text = comboBox1.Text;
+                        }));
+                    });
+                    th2.Start();
+                }
+            });
+            th.Start();
+
+        }
+
+        private void ModernButton2_Click(object sender, EventArgs e)
+        {
+            ModifiableAction.SceneName = comboBox1.Text;
+            ModifiableAction.SceneItem = comboBox2.Text;
+            CloseWithResult(DialogResult.OK);
+        }
+
+        private void ModernButton3_Click(object sender, EventArgs e)
+        {
+            CloseWithResult(DialogResult.Cancel);
+        }
+    }
+}
